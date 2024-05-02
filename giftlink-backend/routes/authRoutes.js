31727d4c910ad02f8f2d3389aa52c169e5654973
Khnzh <n.g.khunjua@gmail.com>
2024-05-02 20:03:57 +0400

    implemented api for updating user

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
 const logger = pino();  // Create a Pino logger instance
+
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -69,9 +70,53 @@ router.post('/login', async (req, res) => {
             const authtoken = jwt.sign(payload, JWT_SECRET);
             logger.info('User blogged in successfully');
             res.json({authtoken, email, name});
-    } catch (e) {
-         return res.status(500).send('Internal server error');
+        } catch (e) {
+            return res.status(500).send('Internal server error');
+        }
+    });
+    
+
+    router.put('/update', async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            logger.error('Validation errors in update request', errors.array());
+            return res.status(400).json({ errors: errors.array() });
     }
+    try {
+        const email = req.headers.email;
+        
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB collection
+        const collection = db.collection("users");
+        const existingUser = await collection.findOne({email: email})
+        
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+    // Task 6: update user credentials in database
+    const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: existingUser },
+        { returnDocument: 'after' }
+    );
+    // Task 7: create JWT authentication using secret key from .env file
+    const payload = {
+        user: {
+            id: updatedUser._id.toString(),
+        },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    res.json({authtoken});
+
+} catch (e) {
+     return res.status(500).send('Internal server error');
+
+}
 });
 
 module.exports = router;
